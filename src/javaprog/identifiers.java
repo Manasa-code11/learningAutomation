31bb90f533997975d525b4e11026607a85103c2b
Manasa-code11 <manasa.kanaparthy@gmail.com>
2025-06-11 14:44:47 +0530

    identifiers

---
+++ src/javaprog/identifiers.java
@@ -0,0 +1,17 @@
+package javaprog;
+
+public class identifiers
+
+{
+    public static void main(String[] args)
+    {
+        int i=10;
+        System.out.print(i);
+        int a=10;
+        a +=20;
+        System.out.println(a);
+        System.out.println("Manasa"+"Rao");
+        System.out.println("Manasa"+"Rao"+10+20);
+        System.out.println(10+40+"Manasa"+"Rao");
+    }
+}
